8f37255cd1818e633b01f54145bff1a012c34543
siddhi9797 <siddhik9797@gmail.com>
2026-04-16 20:45:51 +0530

    adding images

--- src/Events.js
+++ src/Events.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+/* import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./events.css";
 
@@ -32,6 +32,74 @@ function Events() {
           </div>
         ))}
 
+      </div>
+    </div>
+  );
+}
+
+export default Events;
+
+*/
+
+
+
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./events.css";
+
+// import all images
+import event1 from "../images/event1.jpg";
+import event2 from "../images/event2.jpg";
+
+function Events() {
+
+  const [events, setEvents] = useState([]);
+
+  // map image names to actual imports
+  const imageMap = {
+    "event1.jpg": event1,
+    "event2.jpg": event2
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://sheetdb.io/api/v1/2tm8cjnqfsjj3")
+      .then((response) => {
+        setEvents(response.data);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Upcoming Events</h1>
+
+      <div className="event-grid">
+        {events.map((event, index) => (
+          <div className="event-card" key={index}>
+
+            {/* Show Image */}
+            {event.image && (
+              <img
+                src={imageMap[event.image] || event.image}
+                alt="event"
+                className="event-image"
+              />
+            )}
+
+            {/* Show other fields */}
+            {Object.keys(event).map((key) => {
+              if (key === "image") return null; // skip image field
+
+              return (
+                <p key={key}>
+                  <strong>{key}:</strong> {event[key]}
+                </p>
+              );
+            })}
+
+          </div>
+        ))}
       </div>
     </div>
   );
